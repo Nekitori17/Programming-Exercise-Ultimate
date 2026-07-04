a3b426f74c55fc557966c5469b59d3eba8f1f1e3
Nekitori17 <cuongnguyen286641@gmail.com>
2026-07-05 00:23:50 +0700

    first commit

---
+++ template.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  freopen("input.inp", "r", stdin);
+  freopen("output.out", "w", stdout);
+
+  return 0;
+};
